0d0cf360f322660a8da9baf6663c0c377108bc18
Kevin Foong <kevin9foong@gmail.com>
2021-07-18 15:55:45 +0800

    Format Calendar Screen test

--- src/screens/home/__tests__/CalendarScreen.test.tsx
+++ src/screens/home/__tests__/CalendarScreen.test.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { fireEvent, render } from '@testing-library/react-native';
 import { createStore } from 'redux';
-import rootReducer from "../../../redux/reducers";
+import rootReducer from '../../../redux/reducers';
 import CalendarScreen from '../CalendarScreen';
 
 
 test('Renders calendar card correctly according to data', () => {
-  const cards = [
-    {
-      title: 'Task Number 1',
-      description: 'Task 1 details',
-      startdate: new Date(),
-      duedate: new Date(),
-      uid: ''
-    }, 
-  ];
+	const cards = [
+		{
+			title: 'Task Number 1',
+			description: 'Task 1 details',
+			startdate: new Date(),
+			duedate: new Date(),
+			uid: ''
+		}, 
+	];
 
-  const store = createStore(rootReducer, { cards: cards });
+	const store = createStore(rootReducer, { cards: cards });
 
-  const { getByText, debug } = render(
-  <Provider store={store}>
-    <CalendarScreen />
-  </Provider>
+	const { getByText, debug } = render(
+		<Provider store={store}>
+			<CalendarScreen />
+		</Provider>
 	);
 
-  debug()
+	debug();
 
-  expect(getByText('Task Number 1')).toBeTruthy();
-})
+	expect(getByText('Task Number 1')).toBeTruthy();
+});
